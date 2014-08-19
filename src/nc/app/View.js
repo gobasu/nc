8e3f76e14dfd3ad360527ea79d7c3090610eb012
Dawid Kraczkowski <dawid@w3labs.pl>
2014-08-19 21:08:54 +0200

    View class

--- src/nc/app/View.js
+++ src/nc/app/View.js
@@ -3,17 +3,9 @@ var Application = require('./Application');
 
 var View = util.Class({
     create: function() {
-        this.app = app;
-        this.config = config;
-    },
-    path: function(string) {
-        string = string.replace('%APPDIR%', this.app.dir());
-        string = string.replace('%FWDIR%', Application.FW_DIR);
-        return string;
-    },
-    ready: function() {
-        throw new Error('Extension must implement ready method');
+
     }
+
 });
 
 module.exports = View;
