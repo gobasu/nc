839ff1c0afe93d6ce0200c4c0bbe86a9c106c770
Dawid Kraczkowski <dawid@w3labs.pl>
2014-09-12 12:22:16 +0200

    .

--- sample/app.js
+++ sample/app.js
@@ -1,15 +1,8 @@
 var Application = require('../src/nc').app.Application;
 
-
 var app = new Application();
 app.dir(__dirname);
-
-app.error(function(e, req, res) {
-    console.log('error call');
-    if (res) {
-        console.log(e);
-        res.send('404');
-    }
+app.error(function(err, req, res) {
+    //handle error here
 });
-
 app.run();
